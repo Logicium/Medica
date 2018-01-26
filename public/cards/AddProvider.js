b3f9b402e069d9a666f6469342ce286425cade02
Logicium <Kisora.thomas@gmail.com>
2018-01-26 13:49:55 -0500

    Add provider quality control

--- public/cards/AddProvider.js
+++ public/cards/AddProvider.js
@@ -3,33 +3,45 @@ var AddProvider = function(){
     this.addProviderCard = div();
     this.title = text('Add Provider:','black','20px').css('margin-bottom','20px'),
     this.inputs = list([
-        input('Last Name','text').height('70px'),
-        input('First Name','text').height('70px'),
-        input('Email Address','email').height('70px'),
+        input('Last Name','text').height('70px').prop('required',true),
+        input('First Name','text').height('70px').prop('required',true),
+        input('Email Address','email').height('70px').prop('required',true),
         input('Specialty','text').height('70px'),
         input('Practice Name','text').height('70px')
     ]).css('margin-top','85px');
 
     this.submit = button('Submit').height('50px').css('margin-top','28px').addClass('rev').removeClass('ghost').click(function(){
-        var inputs = $('input');
+        var inputs = $('input').not('.search');
         console.log(inputs);
         var vals = [];
-
+        var complete = true;
         $.each(inputs,function(){
-            console.log(vals.push($(this).val()));
+            if ( $(this).prop('required') && $(this).val() === '' ){
+                $(this).css('border','2px solid #f99');
+                complete = false;
+                swal({title:'Missing field',text:'One or more fields are required are not filled out',type:'error'});
+            };
         });
-        var newData = {
-            last_name:vals[0],
-            first_name:vals[1],
-            email_address:vals[2],
-            specialty:vals[3],
-            practice_name:vals[4]
-        }
+        $.each(inputs,function(){
+            if( complete==true ){ $(this).css('border','1px solid white'); vals.push($(this).val()); $(this).val('') };
+        });
 
-        $('.contentRow').append(new ProviderCard(newData));
-        var currentData = JSON.parse(localStorage.getItem('data'));
-        currentData.push(newData);
-        localStorage.setItem('data',JSON.stringify(currentData));
+        if(complete == true){
+            var newData = {
+                last_name:vals[0],
+                first_name:vals[1],
+                email_address:vals[2],
+                specialty:vals[3],
+                practice_name:vals[4]
+            }
+            $('.contentRow').append(new ProviderCard(newData));
+            var currentData = JSON.parse(localStorage.getItem('data'));
+            currentData.push(newData);
+
+            $('.ascending').replaceWith( new AscendingButton(currentData) );
+            $('.descending').replaceWith( new DescendingButton(currentData) );
+            localStorage.setItem('data',JSON.stringify(currentData));
+        }
     });
 
     return this.addProviderCard.append(
